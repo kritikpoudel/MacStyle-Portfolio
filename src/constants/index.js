b9b4180f7000671b37f11d0412ac74174ef1fc79
kritik poudel <kritikpoudel808@gmail.com>
2025-11-23 16:07:35 +0545

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,76 @@
+const navLinks = [
+  {
+    id: 1,
+    name: "Projects",
+    type: "finder",
+  },
+  {
+    id: 3,
+    name: "Contact",
+    type: "contact",
+  },
+  {
+    id: 4,
+    name: "Resume",
+    type: "resume",
+  },
+];
+
+const navIcons = [
+  {
+    id: 1,
+    img: "/icons/wifi.svg",
+  },
+  {
+    id: 2,
+    img: "/icons/search.svg",
+  },
+  {
+    id: 3,
+    img: "/icons/user.svg",
+  },
+  {
+    id: 4,
+    img: "/icons/mode.svg",
+  },
+];
+
+const dockApps = [
+  {
+    id: "finder",
+    name: "Portfolio", // was "Finder"
+    icon: "finder.png",
+    canOpen: true,
+  },
+  {
+    id: "safari",
+    name: "Articles", // was "Safari"
+    icon: "safari.png",
+    canOpen: true,
+  },
+  {
+    id: "photos",
+    name: "Gallery", // was "Photos"
+    icon: "photos.png",
+    canOpen: true,
+  },
+  {
+    id: "contact",
+    name: "Contact", // or "Get in touch"
+    icon: "contact.png",
+    canOpen: true,
+  },
+  {
+    id: "terminal",
+    name: "Skills", // was "Terminal"
+    icon: "terminal.png",
+    canOpen: true,
+  },
+  {
+    id: "trash",
+    name: "Archive", // was "Trash"
+    icon: "trash.png",
+    canOpen: false,
+  },
+];
+export{navLinks,navIcons,dockApps}
